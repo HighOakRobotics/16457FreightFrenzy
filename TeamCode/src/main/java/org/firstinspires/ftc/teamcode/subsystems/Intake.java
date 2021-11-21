6d5ed4f322907beea69d786caafb016d72cf2c5f
daniel <xwingpilot15@gmail.com>
2021-11-21 16:37:10 -0600

    added a simple intake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.ftc11392.sequoia.subsystem.Subsystem;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class Intake extends Subsystem{
+    DcMotorEx IntakeMotor;
+    @Override
+    public void initialize(HardwareMap hardwareMap) {
+        IntakeMotor = hardwareMap.get(DcMotorEx.class, "intake");
+        IntakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        IntakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        IntakeMotor.setVelocity(0, AngleUnit.DEGREES);
+    }
+
+    @Override
+    public void initPeriodic() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void runPeriodic() {
+        IntakeMotor.setVelocity(360, AngleUnit.DEGREES);
+    }
+
+    @Override
+    public void stop() {
+        IntakeMotor.setVelocity(0);
+    }
+}
